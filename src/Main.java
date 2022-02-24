5691d77d184f14b6e27ddc6b4cadc047ebc734cb
LucyC-1225 <90148310+LucyC-1225@users.noreply.github.com>
2022-02-23 22:37:20 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,79 @@ public class Main {
         boolean exit = false;
 
         while (!exit){
+            System.out.println("________________________________________________");
             System.out.println("What would you like to do?");
             System.out.println("1. Calculate cost of one package");
             System.out.println("2. Simulate packages");
-            System.out.println("3. Exit");
+            System.out.println("3. How package costs are calculated");
+            System.out.println("4. Exit");
             System.out.print("Input: ");
             String choice = sc.nextLine();
+            System.out.println("________________________________________________");
 
             if (choice.equals("1")){
                 System.out.print("Enter the zip code of the origin package: ");
                 String input = sc.nextLine();
                 int zip1 = Integer.parseInt(input);
-                System.out.print("Enter the zp code of the destination package: ");
+                System.out.print("Enter the zip code of the destination package: ");
                 input = sc.nextLine();
                 int zip2 = Integer.parseInt(input);
                 System.out.print("Enter the weight of the package: ");
                 input = sc.nextLine();
                 double weight = Double.parseDouble(input);
-                System.out.println("Cost: " + PostageCalculator.calculatePostage(zip1, zip2, weight));
+                boolean validWeight = false;
+                while(!validWeight){
+                    if (weight >= 0.1){
+                        validWeight = true;
+                    } else {
+                        System.out.println("Invalid weight! The weight must be at least 0.1 pounds!");
+                        System.out.print("Enter the weight of the package: ");
+                        weight = Double.parseDouble(sc.nextLine());
+                    }
+                }
+                System.out.print("Enter the height of the package: ");
+                input = sc.nextLine();
+                double height = Double.parseDouble(input);
+                boolean validHeight = false;
+                while(!validHeight){
+                    if (height >= 2){
+                        validHeight = true;
+                    } else {
+                        System.out.println("Invalid height! The height must be at least 2 inches!");
+                        System.out.print("Enter the height of the package: ");
+                        height = Double.parseDouble(sc.nextLine());
+                    }
+                }
+                System.out.print("Enter the length of the package: ");
+                input = sc.nextLine();
+                double length = Double.parseDouble(input);
+                boolean validLength = false;
+                while(!validLength){
+                    if (length >= 2){
+                        validLength = true;
+                    } else {
+                        System.out.println("Invalid length! The length must be at least 2 inches!");
+                        System.out.print("Enter the length of the package: ");
+                        length = Double.parseDouble(sc.nextLine());
+                    }
+                }
+                System.out.print("Enter the width of the package: ");
+                input = sc.nextLine();
+                double width = Double.parseDouble(input);
+                boolean validWidth = false;
+                while(!validWidth){
+                    if (width >= 2){
+                        validWidth = true;
+                    } else {
+                        System.out.println("Invalid width! The width must be at least 2 inches!");
+                        System.out.print("Enter the width of the package: ");
+                        width = Double.parseDouble(sc.nextLine());
+                    }
+                }
+                System.out.println("Cost: " + PostageCalculator.calculatePostage(zip1, zip2, weight, height, length, width));
+                System.out.println("________________________________________________");
+                System.out.println("Click enter to return to menu");
+                sc.nextLine();
             } else if (choice.equals("2")){
                 System.out.print("How many packages would you like to simulate? ");
                 int num = sc.nextInt();
@@ -39,6 +94,10 @@ public class Main {
                     System.out.println("Package " + (i + 1) + ": ________________________________________________");
                     System.out.println("Origin address: " + packages.get(i).getOrigin().toString());
                     System.out.println("Destination address: " + packages.get(i).getDestination().toString());
+                    System.out.println("Weight: " + packages.get(i).getWeight() + " pounds");
+                    System.out.println("Height: " + packages.get(i).getHeight() + " inches");
+                    System.out.println("Length: " + packages.get(i).getLength() + " inches");
+                    System.out.println("Width: " + packages.get(i).getWidth() + " inches");
                     System.out.println("Cost: " + PostageCalculator.calculatePostage(packages.get(i)));
                     totalCost += PostageCalculator.calculatePostage(packages.get(i));
                 }
@@ -49,6 +108,13 @@ public class Main {
                 sc.nextLine();
 
             } else if (choice.equals("3")){
+                System.out.println("- The base cost of the package is $2.75.");
+                System.out.println("- Every 1/10 pound of the package costs $0.05.");
+                System.out.println("- Packages more than 40 pounds are oversized and will be charged $0.10 per 1/10 pound over the 40 pound limit.");
+                System.out.println("- Any package that exceeds a combined 36 inches will cost an additional 10 cents per inch.");
+                System.out.println("- The difference between the country codes of the origin and destination address divided by 100 is added to the total cost.");
+                System.out.println("- Note that all packages must weigh at least 0.1 pounds and the heights, widths, and lengths of all packages must be at least 2 inches");
+            } else if (choice.equals("4")){
                 exit = true;
             } else {
                 System.out.println("Invalid choice!");
--- src/PostageCalculator.java
+++ src/PostageCalculator.java
@@ -1,28 +1,59 @@
 public class PostageCalculator {
-    public static double calculatePostage(int zip1, int zip2, double weight){
+    public static double calculatePostage(int zip1, int zip2, double weight, double height, double width, double length){
+        //cost of weight
         double cost = 3.75;
-        while (weight >= 0.1){
-            cost += 0.05;
-            weight -= 0.1;
+        if (weight <= 40){
+            cost += weight / 0.1 * 0.05;
+        } else {
+            cost += 40 / 0.1 * 0.05;
+            cost += (weight - 40) / 0.1 * 0.1;
         }
+        //cost of size
+        //what does combined size mean?????
+        double combinedSize = height + width + length;
+        if (combinedSize > 36){
+            cost += (combinedSize - 36) * 0.1;
+        }
+        //cost of zip
         cost = cost + Math.abs(((zip1 / 100) - (zip2 / 100)) / 100.0);
         return (int)((cost + 0.005) * 100) / 100.0;
     }
-    public static double calculatePostage(Address ad1, Address ad2, double weight){
+    public static double calculatePostage(Address ad1, Address ad2, double weight, double height, double width, double length){
+        //cost of weight
         double cost = 3.75;
-        while (weight >= 0.1){
-            cost += 0.05;
-            weight -= 0.1;
+        if (weight <= 40){
+            cost += weight / 0.1 * 0.05;
+        } else {
+            cost += 40 / 0.1 * 0.05;
+            cost += (weight - 40) / 0.1 * 0.1;
+        }
+        //cost of size
+        //what does combined size mean?????
+        double combinedSize = height + width + length;
+        if (combinedSize > 36){
+            cost += (combinedSize - 36) * 0.1;
         }
         cost = cost + Math.abs(((ad1.getZipCode() / 100) - (ad2.getZipCode() / 100)) / 100.0);
         return (int)((cost + 0.005) * 100) / 100.0;
     }
     public static double calculatePostage(Package pack){
+        //cost of weight
         double cost = 3.75;
         double weight = pack.getWeight();
-        while (weight >= 0.1){
-            cost += 0.05;
-            weight -= 0.1;
+        if (weight <= 40){
+            cost += weight / 0.1 * 0.05;
+        } else {
+            cost += 40 / 0.1 * 0.05;
+            cost += (weight - 40) / 0.1 * 0.1;
+        }
+        //cost of size
+        //what does combined size mean?????
+        double height = pack.getHeight();
+        double width = pack.getWidth();
+        double length = pack.getLength();
+        double combinedSize = height + width + length;
+        if (combinedSize > 36){
+            cost += (combinedSize - 36) * 0.1;
         }
         cost = cost + Math.abs(((pack.getOrigin().getZipCode() / 100) - (pack.getDestination().getZipCode() / 100)) / 100.0);
         return (int)((cost + 0.005) * 100) / 100.0;
